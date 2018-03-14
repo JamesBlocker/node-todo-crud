4b0fc8a648a35689ee9e2f69f2914cad3dad635b
Byron <jamesblocker81@gmail.com>
2018-03-14 14:15:27 -0700

    lots of steps

--- models/Todo.js
+++ models/Todo.js
@@ -1,1 +1,10 @@
 // todo model...
+mongoose = require('mongoose')
+todoSchema = new mongoose.Schema({
+    body: String,
+    completed: { type: Boolean, default: false }
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
+
+module.exports = Todo
